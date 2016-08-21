3b1548f2831ef67427e6a3e661dc992c65cb41b8
nickzpapa <nicholas.z.papa@gmail.com>
2016-08-20 19:53:22 -0700

    Added test for quick form fillout

---
+++ scripts/test.js
@@ -0,0 +1,14 @@
+﻿function test() {
+    $("#url").val("https://www.youtube.com/watch?v=df9lJLVyMAE");
+    $("#artist").val("Modest Mouse");
+    $("#album").val("The Fruit That Ate Itself");
+    $("#tracklist").val("Sunspots - 0:00\n"
+		+ "The Waydown - 0:40\n"
+    	+ "Fruit - 3:10\n"
+    	+ "Dirty Fingernails - 4:01\n"
+    	+ "Sunspots in the House of the Late Scapegoat - 7:21\n"
+    	+ "The Fruit That Ate Itself - 10:03\n"
+    	+ "Way Down - 13:23\n"
+    	+ "Summer - 14:05\n"
+    	+ "Karma's Payment - 17:17");
+}
